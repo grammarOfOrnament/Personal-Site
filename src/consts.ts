50cdf64062294ed2f4fe5ece30b0e554dafc9c9d
jasondipopolo <jasondipopolo@gmail.com>
2023-08-09 09:46:34 -0400

    2nd Attempt to resolve Font 404

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jason DiPopolo';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Jason DiPopolo, UX/UI Designer';
+export const SITE_DESCRIPTION = "Embrace simplicity with a lean UX/UI design approach. Browse Jason DiPopolo's design portfolio, learn more about Jason, and reach out and connect with him!";
